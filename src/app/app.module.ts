25e4348b2eff6db02d6e0bcdd6c2f6469d2f446a
Radu Candea <rfc@exigo.dk>
2021-10-03 11:24:37 +0200

    removed chart code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { Chart } from "chart.js";
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -14,8 +12,7 @@ import { HomeComponent } from './home/home.component';
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    Chart
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
